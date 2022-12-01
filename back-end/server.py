432ceadc6eb414f7cedf82419ddef31b71f25cb6
Marshall-Artz <marshallarts12@gmail.com>
2022-11-30 20:57:29 -0600

    basic rest endpoint

---
+++ back-end/server.py
@@ -0,0 +1,15 @@
+# save this as app.py
+from flask import Flask
+from flask_restful import Api, Resource
+
+app = Flask(__name__)
+api = Api(app)
+
+class HelloWorld(Resource):
+    def get(self):
+        return {"data": "Hello World!"}
+
+api.add_resource(HelloWorld, "/helloWorld")
+
+if __name__ == "__main__":
+    app.run(debug=True)
