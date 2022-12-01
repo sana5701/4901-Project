3c6c2a1415a5e33d4f8141f684198db96b4c48a2
Marshall-Artz <marshallarts12@gmail.com>
2022-12-01 09:18:59 -0600

    changing frontend to use button

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,17 +2,21 @@ import './App.css';
 import Banner from './components/Banner';
 import InputBox from './components/InputBox';
 import OutputBox from './components/OutputBox';
+import GenerateButton from './components/GenerateButton';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Banner/> 
-        <InputBox/> 
-        <OutputBox/>
+        <div className='flexbox-container'>
+          <InputBox/> 
+          <GenerateButton />
+          <OutputBox/>
+        </div>
       </header>
     </div>
   );
-} 
+}
 
 export default App;
