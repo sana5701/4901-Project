ef618b301a8a2b73de9f388ef49372bda52473b1
Marshall-Artz <marshallarts12@gmail.com>
2022-12-01 18:29:43 -0600

    backend and frontend communicating

--- front-end/src/components/GenerateButton.jsx
+++ front-end/src/components/GenerateButton.jsx
@@ -5,8 +5,16 @@ import IconButton from '@mui/material/IconButton';
 import DoubleArrowIcon from '@mui/icons-material/DoubleArrow';
 
 const GenerateButton = () => {
+  function getCode(description) {
+    let response = fetch(
+        `http://127.0.0.1:5000/helloWorld`
+    ).then((response) => response.json()).then((responseJson) => {
+      console.log(responseJson)
+    });
+  }
+
   return (
-    <IconButton className="GenerateButton" aria-label="generate code" color="primary" size="large">
+    <IconButton onClick={getCode} className="GenerateButton" aria-label="generate code" color="primary" size="large">
         <DoubleArrowIcon />
     </IconButton>
   );
